9a2d834ecd4deb7fa0a5793908903901a5cb8c49
laurynasra <ragauskas.laurynas@gmail.com>
2023-02-04 19:44:51 +0200

    WIP refactor 01

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -1,15 +1,18 @@
+use std::env;
 use std::fs::File;
-use std::io::{self, BufRead, Read};
-use std::path::{self, Path};
+use std::io::{self, BufRead};
+use std::path::Path;
 
 fn main() {
+    let args = env::args();
+    let path = args[0];
     if let Ok(lines) = read_lines("./data.txt") {
         let mut sums: Vec<u32> = vec![];
         let mut sum: u32 = 0;
         for line in lines {
             if let Ok(value) = line {
                 println!("{}", value);
-                if (value == "") {
+                if value == "" {
                     println!("sum: {}", sum);
                     sums.push(sum);
                     sum = 0;
@@ -21,10 +24,10 @@ fn main() {
         }
         sums.sort();
         sums.reverse();
-        let maxValue = *sums.iter().max().unwrap();
+        let max_value = *sums.iter().max().unwrap();
         let top3_vec: Vec<u32> = vec![sums[0], sums[1], sums[2]];
         let top_3_sum: u32 = top3_vec.iter().sum();
-        println!("max: {}", maxValue);
+        println!("max: {}", max_value);
         println!("sum top 3: {}", top_3_sum);
     }
 }
