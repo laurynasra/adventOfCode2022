702debd8d5c0cb5d3f91ef1fb026e183c0c78975
laurynasra <ragauskas.laurynas@gmail.com>
2023-02-05 16:59:34 +0200

    Added handling of draw

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use phf::phf_map;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -8,10 +9,20 @@ struct Args {
     path: String,
 }
 
+static HAND_SCORES: phf::Map<&str, i32> = phf_map! {
+    "X" => 1, // Rock
+    "A" => 1,
+    "Y" => 2, // Paper
+    "B" => 2,
+    "Z" => 3, // Scissors
+    "C" => 3,
+};
+
 fn main() {
     let args = Args::parse();
     let path = args.path;
     if let Ok(lines) = read_lines(path) {
+        let mut my_score = 0;
         for line in lines {
             if let Ok(value) = line {
                 let v: Vec<String> = value
@@ -20,8 +31,19 @@ fn main() {
                     .map(|val| val.parse().unwrap())
                     .collect();
                 println!("pairs: {}, {}", v[0], v[1]);
+                let first_hand: String = v[0].to_string();
+                let second_hand: String = v[1].to_string();
+                // lets handle draw first
+                if HAND_SCORES.get(&first_hand.to_string()).unwrap()
+                    == HAND_SCORES.get(&second_hand).unwrap()
+                {
+                    my_score += 3; // draw gives me 3 points
+                                   // add my hand's score
+                    my_score += HAND_SCORES.get(&(second_hand.to_string())).unwrap();
+                }
             }
         }
+        println!("My score: {}", my_score);
     }
 }
 
