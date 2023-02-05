ceafae2069c0939dcf3f3bb52fa9a0ddd56c3673
laurynasra <ragauskas.laurynas@gmail.com>
2023-02-05 19:44:00 +0200

    WIP 02 part two

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -18,11 +18,19 @@ static HAND_SCORES: phf::Map<&str, i32> = phf_map! {
     "C" => 3,
 };
 
+static GAME_END_SCORES: phf::Map<&str, i32> = phf_map! {
+    "DRAW" => 3,
+    "WIN" => 6,
+    "LOSE" => 0,
+};
+
 fn main() {
     let args = Args::parse();
     let path = args.path;
+
     if let Ok(lines) = read_lines(path) {
         let mut my_score = 0;
+        let mut my_score2 = 0;
         for line in lines {
             if let Ok(value) = line {
                 let v: Vec<String> = value
@@ -34,13 +42,60 @@ fn main() {
                 let first_hand: String = v[0].to_string();
                 let second_hand: String = v[1].to_string();
                 // lets handle draw first
+                match second_hand.as_str() {
+                    "X" => { // Lose
+                    }
+                    "Y" => {
+                        // Draw
+                        my_score2 += GAME_END_SCORES.get("DRAW").unwrap();
+                        my_score2 += HAND_SCORES.get(&first_hand.to_string()).unwrap();
+                    }
+                    "Z" => {}
+                    _ => {}
+                }
+
                 if HAND_SCORES.get(&first_hand.to_string()).unwrap()
                     == HAND_SCORES.get(&second_hand).unwrap()
                 {
                     my_score += 3; // draw gives me 3 points
                                    // add my hand's score
                     my_score += HAND_SCORES.get(&(second_hand.to_string())).unwrap();
+                    continue;
                 }
+
+                match first_hand.as_str() {
+                    "A" => {
+                        match second_hand.as_str() {
+                            "Y" => {
+                                // I won
+                                my_score += 6;
+                            }
+                            _ => {}
+                        }
+                    }
+                    "B" => {
+                        match second_hand.as_str() {
+                            "Z" => {
+                                // I won
+                                my_score += 6;
+                            }
+                            _ => {}
+                        }
+                    }
+                    "C" => {
+                        match second_hand.as_str() {
+                            "X" => {
+                                // I won
+                                my_score += 6;
+                            }
+                            _ => {}
+                        }
+                    }
+                    _ => {
+                        println!("something else");
+                    }
+                }
+                my_score += HAND_SCORES.get(&second_hand.to_string()).unwrap();
             }
         }
         println!("My score: {}", my_score);
