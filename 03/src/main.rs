52917408b65a8bac57795c9fff6b3350e3cd7a07
laurynasra <ragauskas.laurynas@gmail.com>
2023-02-16 16:47:06 +0200

    Solved 03 part 2

--- 03/src/main.rs
+++ 03/src/main.rs
@@ -15,31 +15,40 @@ fn main() {
         .chars()
         .collect();
     let mut sum = 0;
+    let mut sum2 = 0;
+    let mut groups: Vec<String> = vec![];
     if let Ok(lines) = read_lines(path) {
         for line in lines {
             if let Ok(value) = line {
+                groups.push(value.clone());
+                if groups.len() == 3 {
+                    let mut uniq_group: Vec<char> = groups[0].chars().collect::<Vec<_>>();
+                    uniq_group.sort_unstable();
+                    uniq_group.dedup();
+                    for c2 in uniq_group {
+                        if groups[1].contains(c2) && groups[2].contains(c2) {
+                            let score = alphabet.iter().position(|&l| l == c2).unwrap() + 1;
+                            sum2 += score;
+                        }
+                    }
+                    groups.clear();
+                }
                 let line_lenght = value.len();
                 let half_size = line_lenght / 2;
                 let split = value.split_at(half_size);
-                println!("{} has {} {} parts", value, split.0, split.1);
-                for c in split.0.chars() {
-                    println!("Processsing {} of {}", c, split.0);
+                let mut uniq_split: Vec<char> = split.0.chars().collect::<Vec<_>>();
+                uniq_split.sort_unstable();
+                uniq_split.dedup();
+                for c in uniq_split {
                     if split.1.contains(c) {
                         let score = alphabet.iter().position(|&l| l == c).unwrap() + 1;
-                        //println!("Duplicate {} in {}", c, value);
-                        println!(
-                            "value of duplicate {} is {}, sum: {}",
-                            c,
-                            alphabet.iter().position(|&l| l == c).unwrap() + 1,
-                            sum
-                        );
                         sum += score;
                         continue;
                     }
                 }
             }
         }
-        println!("Score: {}", sum);
+        println!("Score: {}, Score 2: {}", sum, sum2);
     }
 }
 
