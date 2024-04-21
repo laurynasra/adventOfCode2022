2e05ad892d7c2748b92ad1ffca8f471e3b8d7cc9
laurynasra <ragauskas.laurynas@gmail.com>
2024-04-21 16:47:05 +0300

    Small fixes

--- 05/src/main.rs
+++ 05/src/main.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 use regex::Regex;
 use std::fs::File;
 use std::io::{self, BufRead};
-use std::path::{Path};
+use std::path::Path;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -14,27 +14,25 @@ fn main() {
     let path = args.path;
     let box_re = Regex::new(r"\[(.)\]").unwrap();
     if let Ok(lines) = read_lines(path) {
-        for line in lines {
-            if let Ok(value) = line {
-                    println!("{}", value);
-                let re = Regex::new(r"(...)\s?").unwrap();
-                for caps in re.captures_iter(&value) {
-                    let index = caps
-                        .iter()
-                        .enumerate()
-                        .find(|t| t.1.is_some()) // find the first `Some`
-                        .map(|t| t.0) // extract the index
-                        .unwrap_or(0);
-                    let cap = caps.get(1).unwrap().as_str().to_string();
-                    // skip empty capture
-                    let box_capture = box_re.captures(&cap);
-                    let letter = if let Some(x) = box_capture {
-                        x
-                    } else {
-                        continue;
-                    };
-                    println!("{} {}", letter.get(1).unwrap().as_str().to_string(), index);
-                }
+        for line in lines.into_iter().flatten() {
+            println!("{}", line);
+            let re = Regex::new(r"(...)\s?").unwrap();
+            for caps in re.captures_iter(&line) {
+                let index = caps
+                    .iter()
+                    .enumerate()
+                    .find(|t| t.1.is_some()) // find the first `Some`
+                    .map(|t| t.0) // extract the index
+                    .unwrap_or(0);
+                let cap = caps.get(1).unwrap().as_str().to_string();
+                // skip empty capture
+                let box_capture = box_re.captures(&cap);
+                let letter = if let Some(x) = box_capture {
+                    x
+                } else {
+                    continue;
+                };
+                println!("{} {}", letter.get(1).unwrap().as_str(), index);
             }
         }
     }
