eefd08a022945cb99efb33fe3fe6927308bda256
laurynasra <ragauskas.laurynas@gmail.comn>
2024-10-06 11:22:37 +0300

    WIP 05

--- 05/src/main.rs
+++ 05/src/main.rs
@@ -13,29 +13,51 @@ struct Args {
 fn main() {
     let args = Args::parse();
     let path = args.path;
-    let mut map: HashMap<i32, Vec<char>> = HashMap::new();
-    let re = Regex::new(r"(\w)").unwrap();
-    if let Ok(lines) = read_lines(path) {
-        for line in lines.into_iter().flatten() {
-            // Detects ending of the intial boxes configuration
-            if line.starts_with(' ') && line.chars().nth(1).unwrap() == '1' {
-                break;
-            }
-            let line_split = line
-                .chars()
-                .collect::<Vec<char>>()
-                .chunks(4)
-                .map(|c| c.iter().collect::<String>())
-                .collect::<Vec<String>>();
+    let mut map: HashMap<usize, Vec<String>> = HashMap::new();
+    let box_letter_regex = Regex::new(r"(\w)").unwrap();
+    let mut lines = read_lines(path).unwrap();
+    for line in lines.by_ref().flatten() {
+        // Detects ending of the intial boxes configuration by finding " 1" string
+        if line.starts_with(" 1") {
+            break;
+        }
+        let line_split = line
+            .chars()
+            .collect::<Vec<char>>()
+            .chunks(4)
+            .map(|c| c.iter().collect::<String>())
+            .collect::<Vec<String>>();
 
-            for (pos, split) in line_split.iter().enumerate() {
-                // Skip the empty spot in the boxes stash
-                if split == "    " {
-                    continue;
-                }
+        for (pos, split) in line_split.iter().enumerate() {
+            // Skip the empty spot in the boxes stash
+            if split == "    " {
+                continue;
             }
+            let m = box_letter_regex.captures(split).unwrap();
+            let vec = match map.get_mut(&pos) {
+                Some(v) => v,
+                None => {
+                    let v: Vec<String> = Vec::new();
+                    map.insert(pos, v);
+                    map.get_mut(&pos).unwrap()
+                }
+            };
+            vec.push(m[0].to_string());
         }
     }
+    let vec = Some(map).unwrap();
+    println!("{:?}", vec.get(&0_usize).unwrap());
+    let direction_regex = Regex::new(r"(\d)").unwrap();
+    for line in lines.flatten() {
+        println!("{:?}", line.to_string());
+        if line.eq("") {
+            continue;
+        }
+        let ds = direction_regex.captures(line.as_str()).unwrap();
+        println!("{:?}", ds);
+        break;
+    }
+    //  start moving box
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
