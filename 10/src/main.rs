d6f87eff9b7964aebc976f86ae3ff5fe40ee3f53
laurynasra <ragauskas.laurynas@gmail.com>
2025-04-23 14:37:51 +0300

    10

--- 10/src/main.rs
+++ 10/src/main.rs
@@ -9,11 +9,11 @@ struct Args {
     path: String,
 }
 
-fn get_op_and_args(line: &str) -> (&str, i8) {
+fn get_op_and_args(line: &str) -> (&str, i16) {
     let s: Vec<&str> = line.split(' ').collect();
     match s.len() {
         2 => {
-            let arg:i8 = s[1].parse().unwrap();
+            let arg:i16 = s[1].parse().unwrap();
             return ("addx", arg);
         },
         1 => ("noop", 0),
@@ -27,29 +27,38 @@ fn main() {
     let path = args.path;
     let mut lines = read_lines(path).unwrap();
     let mut cycle = 0;
-    let mut x = 1;
+    let mut x = 1i16;
     let checkpoints:Vec<i16> = vec![20, 60, 100, 140, 180, 220];
-    let mut buffer:Vec<&str> = Vec::new();
+    let mut buffer = String::new();
     // let mut signal_strengths= Vec::new();
     for line in lines.map_while(Result::ok) {
         let (op, arg) = get_op_and_args(&line);
+        let sprite:Vec<i16> = [x-1, x, x+1].to_vec();
         match op {
             "addx" => {
-                let mut a = "";
-                if (x + 2i8).abs_diff(cycle).lt(&2u8) {
-                    buffer.push("#");
+                if sprite.contains(&cycle) {
+                    buffer.push('#');
                 } else {
-                    buffer.push(".");
+                    buffer.push('.');
                 }
 
                 // if checkpoints.contains(&cycle) {
                 //     signal_strengths.push(cycle * x);
                 // }
                 cycle += 1;
-                if (x + 2).abs_diff(cycle).lt(&2u8) {
-                    buffer.push("#");
+                if (cycle == 40) {
+                    cycle = 0;
+                    // x = 1;
+                }
+                if sprite.contains(&cycle) {
+                    buffer.push('#');
                 } else {
-                    buffer.push(".");
+                    buffer.push('.');
+                }
+                cycle += 1;
+                if (cycle == 40) {
+                    cycle = 0;
+                    // x = 1;
                 }
                 // if checkpoints.contains(&cycle) {
                 //     signal_strengths.push(cycle * x);
@@ -57,11 +66,16 @@ fn main() {
                 x += arg;
             }
             "noop" => {
-                cycle += 1;
-                if (x + 2).abs_diff(cycle).lt(&2u8) {
-                    buffer.push("#");
+
+                if sprite.contains(&cycle) {
+                    buffer.push('#');
                 } else {
-                    buffer.push(".");
+                    buffer.push('.');
+                }
+                cycle += 1;
+                if (cycle == 40) {
+                    cycle = 0;
+                    // x = 1;
                 }
                 // if checkpoints.contains(&cycle) {
                 //     signal_strengths.push(cycle * x);
@@ -69,6 +83,11 @@ fn main() {
             }
             _ => continue,
         }
+        // println!("cycle: {}, sprite : {:?}, x: {}, line: {:?}", cycle, sprite, x, buffer);
+        // if (cycle == 40) {
+        //     cycle = 0;
+        //     // x = 1;
+        // }
         // println!("State after executing {} {}", op, arg);
         // println!("x={} cycle={}", x, cycle);
     }
@@ -77,7 +96,17 @@ fn main() {
     //         .collect::<Vec<String>>(){
     //     println!("{}", line);
     // }
-    println!("{:?}", buffer);
+    // println!("{}", buffer);
+    let mut final_vector_value:Vec<String> = Vec::new();
+    while(buffer.len() > 40) {
+        let cutoff = buffer.split_off(40);
+        final_vector_value.push(buffer);
+        buffer = cutoff;
+    }
+    for line in final_vector_value {
+        println!("{}", line);
+    }
+    println!("{}", buffer);
     // println!("Signal strengths={:?}", signal_strengths);
     // println!("Total signal strengths={:?}", signal_strengths.iter().sum::<i16>());
 }
